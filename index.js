ade4e8523c9f8ae05f13571329baeb6a314fa302
kostiantynkovalchuk <kostiantynkovalchuk32@gmail.com>
2024-11-06 16:44:16 +0100

    score  changed

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ let guestScore = 0;
 
 // Function to increase score
 function increaseScore(team) {
-    if (team === "home") {
-        homeScore += 1;
-        document.getElementById("home-score").textContent = homeScore;
-    } else if (team === "guest") {
-        guestScore += 1;
-        document.getElementById("guest-score").textContent = guestScore;
+  if (team === "home") {
+    if (homeScore < 99) {
+      homeScore += 1;
+      document.getElementById("home-score").textContent = homeScore;
+    } else {
+      homeScore = 0;
     }
+  } else if (team === "guest") {
+    if (guestScore < 99) {
+      guestScore += 1;
+      document.getElementById("guest-score").textContent = guestScore;
+    } else {
+      guestScore = 0;
+    }
+  }
 }
 
 // Function to reset scores
 function resetScores() {
-    homeScore = 0;
-    guestScore = 0;
-    document.getElementById("home-score").textContent = homeScore;
-    document.getElementById("guest-score").textContent = guestScore;
+  homeScore = 0;
+  guestScore = 0;
+  document.getElementById("home-score").textContent = homeScore;
+  document.getElementById("guest-score").textContent = guestScore;
 }
